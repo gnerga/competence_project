b641d9338a47e963fdc8b9f8235fe08d584ec86f
Bartosz Paluszkiewicz <bomblozaurus@gmail.com>
2021-01-04 23:08:08 +0100

    add mysql connector; read all users;

--- crud/src/main/java/Main.java
+++ crud/src/main/java/Main.java
@@ -1,7 +1,26 @@
-import java.sql.Connection;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection =
+        String url = "jdbc:mysql://127.0.0.1:3307/GoodHotDog";
+        String user = "root";
+        String password = "root";
+
+        String selectUsers = "SELECT * FROM users;";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            Statement statement = connection.createStatement();
+            ResultSet users = statement.executeQuery(selectUsers);
+
+            while(users.next()){
+                int id = users.getInt("id");
+                String phoneNumber = users.getString("phone_number");
+                String profile = users.getString("profile");
+
+                System.out.println("User{id: " + id + ", phoneNumber: " + phoneNumber + ", profile: " + profile);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
