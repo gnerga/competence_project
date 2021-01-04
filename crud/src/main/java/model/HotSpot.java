47a27b4043977af67c7f65f7f03a89334237e7a4
stanislawr <stas.rojek132@gmai.com>
2021-01-04 23:06:12 +0100

    add User.java add HotSpot.java

---
+++ crud/src/main/java/model/HotSpot.java
@@ -0,0 +1,37 @@
+package model;
+
+public class HotSpot {
+    public final String name;
+    public final String description;
+    public final float longitude;
+    public final float latitude;
+    public final String type;
+
+    public HotSpot(String name, String description, float longitude, float latitude, String type) {
+        this.name = name;
+        this.description = description;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
---
+++ crud/src/main/java/model/User.java
@@ -0,0 +1,25 @@
+package model;
+
+public class User {
+    public final int id;
+    public final String phoneNumber;
+    public final String profile;
+
+    public User(int id, String phoneNumber, String profile) {
+        this.id = id;
+        this.phoneNumber = phoneNumber;
+        this.profile = profile;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+}
