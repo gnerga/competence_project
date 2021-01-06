04a4f5048ac1fc2be408d75d7b421dddcb359d7f
Grzegorz <gregnerga@gmail.com>
2021-01-06 21:19:12 +0100

    add CLI to generator, hotspot generator to script

---
+++ data_generator.py
@@ -0,0 +1,122 @@
+import generate_user as user_generator
+import generate_hotspot as hotspot_generator
+import generate_log as log_generator
+
+import datetime
+import pandas as pd
+import argparse
+
+
+HOTSPOT_FILENAME = 'hotspot.csv'
+
+
+def data_to_csv(list, filename, headers):
+    data = []
+    for x in list:
+        data.append(x.to_list())
+
+    df = pd.DataFrame(data, columns=headers)
+    df.to_csv(filename, index=False, header=True)
+
+
+def traces_to_csv(logs, filename, headers, duration=False):
+    data = []
+    for x in logs:
+        x.format_enter_and_exit_time_and_duration_to_iso_format()
+        if duration:
+            data.append(x.to_list())
+        else:
+            data.append(x.to_list_without_duration())
+
+    df = pd.DataFrame(data, columns=headers)
+    df.to_csv(filename, index=False, header=True)
+
+
+def main():
+
+    user_file_location_path = None
+    hotspot_file_location_path = None
+    trace_file_location_path = None
+
+    start_date = datetime.datetime(year=2020, month=10, day=1, hour=7, minute=0, second=0)
+    end_date = datetime.datetime(year=2020, month=12, day=1, hour=22, minute=0, second=0)
+    number_of_user = 150
+
+    if hotspot_file_location_path:
+        hotspots_filename = hotspot_file_location_path
+    else:
+        hotspots_filename = "hotspot" + ".csv"
+
+    if trace_file_location_path:
+        traces_filename = trace_file_location_path
+    else:
+        traces_filename = "log_" + str(number_of_user)
+
+    if user_file_location_path:
+        users_filename = user_file_location_path
+    else:
+        users_filename = "user_"+ ".csv"
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-su', "--setuserpath", dest='setuserpath', help="Set path to users file")
+    parser.add_argument('-sh', "--sethotpath", dest='sethotpath', help="Set path to hotspot file")
+    parser.add_argument('-st', "--settracepath", dest='settracepath', help="Set trace to hotspot file")
+
+    parser.add_argument("-u", "--users", dest='users', help="Number of users to generate", type=int)
+    parser.add_argument("-hs", "--hotspots",  help="Generate hotspots", action='store_true', default=False)
+    parser.add_argument("-l", "--load",  help="Load users and hotspots from file", action='store_true', default=False)
+
+    args = parser.parse_args()
+
+    hotspots = []
+    users = []
+
+    if args.setuserpath is not None:
+        print("Path user set !")
+        user_file_location_path = str(args.setuserpath)
+
+    if args.sethotpath is not None:
+        print("Path hotspot set !")
+        hotspot_file_location_path = str(args.sethotpath)
+
+    if args.settracepath is not None:
+        print("Path trace set !")
+        trace_file_location_path = str(args.settracepath)
+
+    if args.hotspots:
+        print("Hotspots generating...")
+        hotspot_generator.generateHotspotPoint(51.74732490481434, 19.453798422308278, hotspots_filename)
+        hotspots = hotspot_generator.return_list_of_hot_spots(hotspots_filename)
+
+    if args.users is not None:
+        print("Users generating...")
+        number_of_user = args.users
+        users = user_generator.generate_list_of_user(args.users)
+        data_to_csv(users, filename=users_filename, headers=["Id", "PhoneNumber", "Profile"])
+
+    if args.load:
+        print("Loading...")
+        users = user_generator.generate_list_of_user(number_of_user)
+        hotspots = hotspot_generator.return_list_of_hot_spots(hotspots_filename)
+        print("Done")
+
+    if hotspots and users:
+        logs = log_generator.generate_log_files(number_of_user, hotspots, start_date, end_date)
+
+        traces_to_csv(logs, filename=traces_filename + ".csv",
+                      headers=["UserId", "PoisName", "EnterTime", "ExitTime"])
+
+        for log in logs:
+            log.count_duration()
+
+        traces_to_csv(logs, filename=traces_filename+"_duration.csv",
+                      headers=["UserId", "PoisName", "EnterTime", "ExitTime", "Duration"], duration=True)
+    else:
+        print("There is no hotspots or user data ...")
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ generate_hotspot.py
@@ -0,0 +1,58 @@
+import math
+import random
+import pandas as pd
+import csv
+from models.Point import Point
+from models.HotSpot import HotSpot
+
+#CDF  cumulative distribution function ( number > 0)
+def reverseCDF (number):
+    return math.pow(math.log(1 - number), 2)
+
+
+def generateHotspotPoint(centerLat, centerLong, filename):
+   
+    numberOfPoints = 10
+    centerLat = centerLat * math.pi / 180.0
+    centerLong = centerLong * math.pi / 180.0
+    earthR = 6371000
+    points = list()
+
+    for i in range(numberOfPoints):
+        functionArgument = 1.0 / (numberOfPoints + 1) * (i + 1)
+        angleRandom = (random.random() * 2 - 1.0) * 2 * math.pi
+        randomDistance = reverseCDF(functionArgument) * 1000
+        latitude = math.asin((math.sin(centerLat) * math.cos(randomDistance / earthR)) + (math.cos(centerLat) * math.sin(randomDistance / earthR)*math.cos(angleRandom))) * 180 / math.pi
+        longitude =  (centerLong  + math.atan2(math.sin(angleRandom) * math.sin(randomDistance / earthR) * math.cos(centerLat),math.cos(randomDistance / earthR) - (math.sin(centerLat) * math.sin(latitude)))) * 180 / math.pi
+        points.append(Point(latitude,longitude))
+                                    
+
+
+    with open(filename, mode='w') as hotspot_file:
+        hotspot_writer = csv.writer(hotspot_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        hotspot_writer.writerow(['Cafeteria','Place for rest between lectures',str(points[0].x),str(points[0].y),'indoor'])
+        hotspot_writer.writerow(['Main Library','The biggest library on TUL',str(points[1].x),str(points[1].y),'indoor'])
+        hotspot_writer.writerow(['Park','Good place for rest brain before exams',str(points[2].x),str(points[2].y),'outdoor'])
+        hotspot_writer.writerow(['Finestra Pub','Pizzeria which is a good friend of the TUL',str(points[3].x),str(points[3].y),'indoor'])
+        hotspot_writer.writerow(['Statue before BAIS Faculty','Statue presenting hand',str(points[4].x),str(points[4].y),'outdoor'])
+        hotspot_writer.writerow(['Bridge','Bridge between BAIS and CTI buildings',str(points[5].x),str(points[5].y),'outdoor'])
+        hotspot_writer.writerow(['Chinese Bar','Place with cheap dinners',str(points[6].x),str(points[6].y),'indoor'])
+        hotspot_writer.writerow(['Information Technology Center','Place where most od TUL servers are hosted',str(points[7].x),str(points[7].y),'indoor'])
+        hotspot_writer.writerow(['Deans office','Place where students are submitting various applications',str(points[8].x),str(points[8].y),'indoor'])
+        hotspot_writer.writerow(['Sports bay','Place where most sport activities have place',str(points[9].x),str(points[9].y),'indoor'])
+
+
+def return_list_of_hot_spots(file):
+    list_of_hot_spots = []
+    df = pd.read_csv(file, delimiter=';', header=None)
+
+    for index, row in df.iterrows():
+        list_of_hot_spots.append(HotSpot(index+1, row[0], row[1], row[2], row[3], row[4]))
+        index = index + 1
+    return list_of_hot_spots
+
+#     generateHotspotPoint(51.74732490481434, 19.453798422308278, 'hotspot.csv')
+
+
+
+
